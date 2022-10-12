671188cf6dce161a7c2ad703d41335344a649eaa
todelusion <todelusion@gmail.com>
2022-10-12 16:32:44 +0800

    LandingPage/ ui styling

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,9 +1,47 @@
-import React from "react";
+import { motion, useScroll } from "framer-motion";
+import logoPath from "../assets/LOGO.png";
+import arrowDownPath from "../assets/arrow_down.svg";
 
-type Props = {};
+const LandingPage = (): JSX.Element => {
+  const { scrollYProgress } = useScroll();
+  console.log(scrollYProgress);
 
-const LandingPage = (props: Props) => {
-  return <div>LandingPage</div>;
+  return (
+    <div className="font-serif text-white">
+      <nav className="absolute px-4 pt-8">
+        <img src={logoPath} alt="logo" className="w-20" />
+      </nav>
+      <section className="flex-center relative min-h-screen font-semibold">
+        <p>
+          <span className="mb-2 block font-light">國立東華大學VIP課程</span>
+          <span className="text-3xl">移工與她的老人</span>
+          <br />
+          <span className="tracking-wider">Migrant works with the elderly</span>
+        </p>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatDelay: 0.5,
+            repeatType: "reverse",
+            duration: 0.5,
+          }}
+        >
+          <img
+            src={arrowDownPath}
+            alt="slide down"
+            className="absolute bottom-5 left-1/2 -translate-x-1/2"
+          />
+        </motion.div>
+      </section>
+      <section className="flex-center min-h-screen font-semibold">
+        <p>test</p>
+      </section>
+    </div>
+  );
 };
 
 export default LandingPage;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,23 +1,19 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{ts,jsx,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{ts,jsx,tsx}"],
   theme: {
     extend: {
       screens: {
-        'xs': "384px"
+        xs: "384px",
       },
       colors: {
-        'primary': '#3F20FF',
-        'second': '#F5F5F5',
-        'third': '#F498D1'
-      }
+        primary: "#FDC6B5 ",
+        second: "#2C3E50",
+      },
     },
     fontFamily: {
-      sans: ['Noto Sans TC','Roboto'],
-      serif: ['Noto Serif TC','Times'],
-      dela: ['Dela Gothic One']
-    }
-  }
-}
+      sans: ["Noto Sans TC", "Roboto"],
+      serif: ["Noto Serif TC", "Times"],
+      dela: ["Dela Gothic One"],
+    },
+  },
+};
