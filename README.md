d87595b4effb912fa4ea543a252b66d8e98c7f47
todelusion <todelusion.gmail.com>
2022-10-30 16:54:37 +0800

    public/main.js / add getter

---
+++ README.md
@@ -0,0 +1,3 @@
+# 新增collection（category）之步驟：
+1. 在`public/admin/config.yml`的collection項新增標籤
+2. 在`public/main.js`的`dirPathArr`物件新增對應的md資料夾路徑
--- public/main.js
+++ public/main.js
@@ -4,9 +4,24 @@ import { readdir, readFile, writeFileSync } from "fs";
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = dirname(__filename);
-const dirPathnotes = join(__dirname, "../content/notes");
+
+const dirPathArr = [
+  {
+    dirName: "activities",
+    get dirPath() {
+      return join(__dirname, `../content/${this.dirName}`);
+    },
+  },
+  {
+    dirName: "notes",
+    get dirPath() {
+      return join(__dirname, `../content/${this.dirName}`);
+    },
+  },
+];
+
 const dirPathPages = join(__dirname, "../src/pages/content");
-let postlist = [];
+
 let pagelist = [];
 
 const months = {
@@ -44,68 +59,71 @@ const formatDate = (date) => {
 };
 
 const getPosts = () => {
-  readdir(dirPath, (err, files) => {
-    if (err) {
-      return console.log("Failed to list contents of directory: " + err);
-    }
-    let ilist = [];
-    files.forEach((file, i) => {
-      let obj = {};
-      let post;
-      readFile(`${dirPath}/${file}`, "utf8", (err, contents) => {
-        const getMetadataIndices = (acc, elem, i) => {
-          if (/^---/.test(elem)) {
-            acc.push(i);
-          }
-          return acc;
-        };
-        const parseMetadata = ({ lines, metadataIndices }) => {
-          if (metadataIndices.length > 0) {
-            let metadata = lines.slice(
-              metadataIndices[0] + 1,
-              metadataIndices[1]
-            );
-            metadata.forEach((line) => {
-              obj[line.split(": ")[0]] = line.split(": ")[1];
+  dirPathArr.forEach((item) => {
+    readdir(item.dirPath, (err, files) => {
+      if (err) {
+        return console.log("Failed to list contents of directory: " + err);
+      }
+      let postlist = [];
+      let ilist = [];
+      files.forEach((file, i) => {
+        let obj = {};
+        let post;
+        readFile(`${item.dirPath}/${file}`, "utf8", (err, contents) => {
+          const getMetadataIndices = (acc, elem, i) => {
+            if (/^---/.test(elem)) {
+              acc.push(i);
+            }
+            return acc;
+          };
+          const parseMetadata = ({ lines, metadataIndices }) => {
+            if (metadataIndices.length > 0) {
+              let metadata = lines.slice(
+                metadataIndices[0] + 1,
+                metadataIndices[1]
+              );
+              metadata.forEach((line) => {
+                obj[line.split(": ")[0]] = line.split(": ")[1];
+              });
+              return obj;
+            }
+          };
+          const parseContent = ({ lines, metadataIndices }) => {
+            if (metadataIndices.length > 0) {
+              lines = lines.slice(metadataIndices[1] + 1, lines.length);
+            }
+            return lines.join("\n");
+          };
+          const lines = contents.split("\n");
+          const metadataIndices = lines.reduce(getMetadataIndices, []);
+          const metadata = parseMetadata({ lines, metadataIndices });
+          const content = parseContent({ lines, metadataIndices });
+          const parsedDate = metadata.date
+            ? formatDate(metadata.date)
+            : new Date();
+          const publishedDate = `${parsedDate["monthName"]} ${parsedDate["day"]}, ${parsedDate["year"]}`;
+          const datestring = `${parsedDate["year"]}-${parsedDate["month"]}-${parsedDate["day"]}T${parsedDate["time"]}:00`;
+          const date = new Date(datestring);
+          const timestamp = date.getTime() / 1000;
+          post = {
+            id: timestamp,
+            title: metadata.title ? metadata.title : "No title given",
+            author: metadata.author ? metadata.author : "No author given",
+            date: publishedDate ? publishedDate : "No date given",
+            time: parsedDate["time"],
+            thumbnail: metadata.thumbnail,
+            content: content ? content : "No content given",
+          };
+          postlist.push(post);
+          ilist.push(i);
+          if (ilist.length === files.length) {
+            const sortedList = postlist.sort((a, b) => {
+              return a.id < b.id ? 1 : -1;
             });
-            return obj;
+            let data = JSON.stringify(sortedList);
+            writeFileSync(`src/db/${item.dirName}.json`, data);
           }
-        };
-        const parseContent = ({ lines, metadataIndices }) => {
-          if (metadataIndices.length > 0) {
-            lines = lines.slice(metadataIndices[1] + 1, lines.length);
-          }
-          return lines.join("\n");
-        };
-        const lines = contents.split("\n");
-        const metadataIndices = lines.reduce(getMetadataIndices, []);
-        const metadata = parseMetadata({ lines, metadataIndices });
-        const content = parseContent({ lines, metadataIndices });
-        const parsedDate = metadata.date
-          ? formatDate(metadata.date)
-          : new Date();
-        const publishedDate = `${parsedDate["monthName"]} ${parsedDate["day"]}, ${parsedDate["year"]}`;
-        const datestring = `${parsedDate["year"]}-${parsedDate["month"]}-${parsedDate["day"]}T${parsedDate["time"]}:00`;
-        const date = new Date(datestring);
-        const timestamp = date.getTime() / 1000;
-        post = {
-          id: timestamp,
-          title: metadata.title ? metadata.title : "No title given",
-          author: metadata.author ? metadata.author : "No author given",
-          date: publishedDate ? publishedDate : "No date given",
-          time: parsedDate["time"],
-          thumbnail: metadata.thumbnail,
-          content: content ? content : "No content given",
-        };
-        postlist.push(post);
-        ilist.push(i);
-        if (ilist.length === files.length) {
-          const sortedList = postlist.sort((a, b) => {
-            return a.id < b.id ? 1 : -1;
-          });
-          let data = JSON.stringify(sortedList);
-          writeFileSync("src/db/posts.json", data);
-        }
+        });
       });
     });
   });
