567dde0b5696c4f554a1ce8211f9beac6a027ecc
todelusion <todelusion.gmail.com>
2022-11-15 18:10:17 +0800

    modified post Page/ thumbnail disable

--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -16,17 +16,17 @@ const Post = (): JSX.Element => {
   if (post === undefined) return <p>重新返回上一頁</p>;
   console.log(post);
   return (
-    <div className="flex-center pt-28">
+    <div className="flex-center select-text pt-28">
       <article className="w-2/3 text-white">
         <h2 className="font-serif text-4xl font-black">{post.title}</h2>
         <ReactMarkdown>{`作者：${post.author}`}</ReactMarkdown>
         <hr className="my-8 h-px border-0 bg-gray-400" />
         <ReactMarkdown className="mb-32">{post.content}</ReactMarkdown>
-        {post.thumbnail !== undefined && (
+        {/* {post.thumbnail !== undefined && (
           <div className="flex-center">
             <img src={post.thumbnail} alt="thumbnail" className="rounded-3xl" />
           </div>
-        )}
+        )} */}
       </article>
     </div>
   );
