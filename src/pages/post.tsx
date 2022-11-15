d112d97a2c632cefd875db4f280d31d22e6622ae
todelusion <todelusion.gmail.com>
2022-11-15 16:32:48 +0800

    add PostContext

--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { BlogProvider } from "../context/PostContext";
-import useBlog from "../hooks/usePost";
+import usePost from "../hooks/usePost";
 
 type Props = {};
 
 const Post = (props: Props) => {
-  const { post } = useBlog();
+  const { post } = usePost();
   console.log(post);
 
   return <h1>test</h1>;
